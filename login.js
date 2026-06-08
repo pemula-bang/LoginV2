db4324dcb77f7c86126dced25ec2437dbcda19a1
pemula-bang <huseininanda44@gmail.com>
2026-06-08 17:37:47 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+//daerah perpindahan halaman---------------------------
+function pindah1() {
+            window.location.href = "mahasiswa1.html";
+}
+
+function pindah2() {
+            window.location.href = "dosen1.html";
+}
+//-----------------------------------------------------
